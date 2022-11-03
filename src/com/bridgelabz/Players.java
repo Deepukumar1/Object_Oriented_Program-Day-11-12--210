2d5cb8aa93fd98515d19c4c46273090c1597a99b
Deepukumar1 <deepukumarmahasheth2@gmail.com>
2022-11-03 13:05:25 +0530

    Added: Deck of cards

---
+++ src/com/bridgelabz/Players.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Players {
+	
+	 public static void main(String[] args) {
+	        System.out.println("\n1.View cards of 4 Players");
+	        System.out.println("Enter Choise :");
+	        DeckOfCards card = new DeckOfCards();
+	        Scanner scanner = new Scanner(System.in);
+	        int num = scanner.nextInt();
+
+	        switch (num) {
+	            case 1:
+	                card.getCardCombination();
+	                card.getPlayer();
+	                break;
+	            default:
+	                System.out.println("!!invalid input!!");
+	                break;
+	        }
+	    }
+
+}
